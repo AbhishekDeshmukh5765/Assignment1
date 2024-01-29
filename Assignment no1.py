c5e7936ecff665b8b82ac732ec264ca5f4081e87
AbhishekDeshmukh5765 <158044457+AbhishekDeshmukh5765@users.noreply.github.com>
2024-01-29 20:01:24 +0530

    Add files via upload

---
+++ Assignment no1.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# 1. In the below elements which of them are values or an expression? eg:- values can be
+#integer or string and expressions will be mathematical operators.
+
+# *=multiply
+# 39=int
+# hello=string
+# -87.8=float
+# -=subtract
+# /=divide
+# +=add
+# 6= int
+
+
+# 2.What is the difference between string and variable?
+# ans-string is type of information and variable is store of information.
+
+# 3.Describe three different data types.
+# Text Type:	str
+# Numeric Types:	int, float
+# Sequence Types:	list, tuple
+# Mapping Type:	dict
+# Set Types:	set
+# Boolean Type:	bool
+
+
+# 4.What is an expression made up of? What do all expressions do?
+# ans-expressions make of combination of operaters those operation produce other value in python.
+
+# 5.This assignment statements, like spam = 10. What is the difference between an
+# expression and a statement?
+# ans-An expression evaluates to a value and A statement executes something.
+
+
+
+# In[1]:
+
+
+# 6. After running the following code, what does the variable bacon contain?
+bacon = 22
+bacon + 1
+# ans-23
+
+
+# In[2]:
+
+
+# 7. What should the values of the following two terms be?
+"spam"+"spamspam"
+
+
+# In[ ]:
+
+
+# 8. Why is eggs a valid variable name while 100 is invalid?
+# because we cant use variable name start with numeric value
+
+
+# 9. What three functions can be used to get the integer, floating-point number, or string
+# version of a value?
+# ans-for integer we use int()
+# for floating-point number we use float()
+# for string we use str()
+
+
+# In[3]:
+
+
+#v10. Why does this expression cause an error? How can you fix it?
+"i have eaten"+99+"burritos"
+
+
+# In[4]:
+
+
+"i have eaten "+"99"+" burritos"
+
+
+# In[ ]:
+
+
+
+
